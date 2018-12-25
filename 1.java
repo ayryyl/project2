11b5269ce0cd61289090cc6e2cbb4a3e0df5520b
myekke <45380934+myekke@users.noreply.github.com>
2018-12-25 11:41:47 +0330

    Update 1.java

--- 1.java
+++ 1.java
@@ -8,7 +8,7 @@ System.out.println (" Please enter number b : ");
 int b = read.nextInt();
 System.out.println (" Please enter number c : ");
 int c = read.nextInt();
-d=Calculations.sun(a,b);
+d=Calculations.sum(a,b);
 System.out.println (d);
 }
 }
