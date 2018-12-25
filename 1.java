29fdb6446e55a8d8be7780bff0bc635a23f83a22
ayryyl <ayryylhashemi@gmail.com>
2018-12-25 10:48:40 +0330

    first commit

---
+++ 1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class A {
+public static void main (String [] args) {
+Scanner read = new Scanner (System.in);
+System.out.println (" Please enter number a : ");
+int a = read.nextInt();
+System.out.println (" Please enter number b : ");
+int b = read.nextInt();
+System.out.println (" Please enter number c : ");
+int c = read.nextInt();
+int d = a+b;
+System.out.print (" The result of the sun of a and b is : ");
+System.out.println ("d");
+}
+}
